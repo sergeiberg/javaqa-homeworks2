670b0846bbc809c0d77024446b8b90e4fbf2257b
fonkisereja <powerpufluvv@gmail.com>
2023-07-08 01:45:39 +0300

    update code

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         int amound = 19_942; //сумма чека билетов
-        int mili = 20; //начисление 1 бонусной мили за каждые ... рублей в чеке
+        int bonus = 20; //начисление 1 бонусной мили за каждые ... рублей в чеке
+        int mili = amound / bonus;
 
-        System.out.println((amound / mili) + " бонусная(-ых/-ые) миля(-ь/-и/)");
+
+        System.out.println(mili + " бонусная(-ых/-ые) миля(-ь/-и/)");
 
     }
 }
