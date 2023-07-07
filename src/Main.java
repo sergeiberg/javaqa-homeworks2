b3c7916ac790c1aaae92aa3a2a1268f8e9a70e1c
fonkisereja <powerpufluvv@gmail.com>
2023-07-08 01:37:57 +0300

    add README

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amound = 19_942; //сумма чека билетов
+        int mili = 20; //начисление 1 бонусной мили за каждые ... рублей в чеке
+
+        System.out.println((amound / mili) + " бонусная(-ых/-ые) миля(-ь/-и/)");
+
+    }
+}
