84ca0f73895cce270163a602530a907c7716e290
fonkisereja <powerpufluvv@gmail.com>
2023-07-12 00:42:14 +0300

    testabilityhw

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int amound = 19_942; //сумма чека билетов
-        int bonus = 20; //начисление 1 бонусной мили за каждые ... рублей в чеке
-        int mili = amound / bonus;
-
-
-        System.out.println(mili + " бонусная(-ых/-ые) миля(-ь/-и/)");
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
